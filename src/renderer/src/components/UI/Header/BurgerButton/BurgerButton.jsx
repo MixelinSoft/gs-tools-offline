0f160cc6bb5290aba48a941c3edfa6470bf058a6
MixelinSoft <127510809+MixelinSoft@users.noreply.github.com>
2024-12-27 09:19:27 +0200

    enhance UI components with box shadows and transition effects; update BurgerButton to toggle aside menu

--- src/renderer/src/components/UI/Header/BurgerButton/BurgerButton.jsx
+++ src/renderer/src/components/UI/Header/BurgerButton/BurgerButton.jsx
@@ -1,9 +1,13 @@
 import styles from './BurgerButton.module.css'
 import { HiOutlineMenuAlt1 } from 'react-icons/hi'
+import { uiActions } from '../../../../../store/slices/uiSlice'
+import { useDispatch } from 'react-redux'
 
 const BurgerButton = () => {
+  // Create Dispatch Function
+  const dispatch = useDispatch()
   const burgerButtonHandler = () => {
-    console.log('Burger button clicked')
+    dispatch(uiActions.asideMenuToggle())
   }
 
   return (
